0303adc98e3f77e20ec16d0792652e5d38235adb
Parth Mangukiya <88765995+20IT066@users.noreply.github.com>
2022-09-07 17:44:51 +0530

    Added Footer in StudentMgmSys project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Naviga from './Components/Naviga';
+import Footer from './Components/Foot';
 
 function App() {
   return (
     <>
     <Naviga/> 
+    <Footer/> 
     </>
   
   );
