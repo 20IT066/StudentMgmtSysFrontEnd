1d809b6d6188c81cc748d51c535202fc2c184a5b
Parth Mangukiya <88765995+20IT066@users.noreply.github.com>
2022-09-07 17:38:54 +0530

    Added Navigation Bar into React App

---
+++ src/Components/Naviga.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+function Naviga() {
+  return (
+    <div>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Add Student</Nav.Link>
+            <Nav.Link href="#features">View Student</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Naviga
