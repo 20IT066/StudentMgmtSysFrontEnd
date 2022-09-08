1c9dd5608ebe406b69564e4d4a4dda73f8bc9de5
Parth Mangukiya <88765995+20IT066@users.noreply.github.com>
2022-09-08 18:21:34 +0530

    Implement add student in react app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Naviga from './Components/Naviga';
 import Footer from './Components/Foot';
+import Student from './Components/Student';
 
 function App() {
   return (
     <>
     <Naviga/> 
     <Footer/> 
+    <Student/>
     </>
   
   );
